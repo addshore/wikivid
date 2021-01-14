491ca2ff9c2a09441bd0046fc9b8f10f2fcb593e
addshore <addshorewiki@gmail.com>
2021-01-14 18:30:58 +0000

    Fine tune the screenshots that are taken

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 let page = '2021_storming_of_the_United_States_Capitol'
 let firstDateToRender = new Date( '2021-01-06T18:34:29Z' )
-let lastDateToRender = new Date( '2021-01-07T06:34:29Z' )// 12 hours later
+let lastDateToRender = new Date( '2021-01-07T00:00:01Z' )// up to midnight..
 //let lastDateToRender = new Date( '2021-01-07T18:34:29Z' )// 24 hours later
 //let page = 'User:Addshore/foo'
 let imageSeconds = 1/30
@@ -30,12 +30,7 @@ var client = new bot({
   debug: false
 });
 
-downloadScreenshotsOfArticleRevisions(page)
-// TODO make gif from images?
-
-function downloadScreenshotsOfArticleRevisions(page) {
-    client.getArticleRevisions(page, handleArticleRevisions);
-}
+client.getArticleRevisions(page, handleArticleRevisions);
 
 function handleArticleRevisions(err, data) {
     if (err) {
@@ -72,16 +67,19 @@ function handleArticleRevision(revision, doneCallback) {
 }
 
 function getScreenshotOfPage(url, saveAs, doneCallback){
-
     // TODO hide annoying elements?
     const optionsSelector = {
+        // If we want to limit what we grab we could use this
         //captureSelector: '#content',
-        screenSize: {
+        customCSS: '.mw-revision, .warningbox { display:none; }',
+        // wait a short time to make sure the page looks as ready as possible
+        renderDelay: 200,
+        windowSize: {
             width: 1920,
             height: 1080
           },
           shotSize: {
-            width: 'all',
+            width: 'window',
             height: 'all'
           },
     };
